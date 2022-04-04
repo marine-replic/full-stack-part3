d49acc1a2072f64399c6c93016f3f0ad88ae1b0c
marine-replic <86975170+marine-replic@users.noreply.github.com>
2022-04-04 14:48:55 -0400

    uibuild

--- part2/phonebook/src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-const Filter = ({ newSearch, handleSearchChange }) => (
-    <form>
-        <div>
-            filter show with <input value={newSearch} onChange={handleSearchChange} />
-        </div>
-    </form>
-)
-
-export default Filter
