1101732849d487d108e1fdad8a5b9c772faaaceb
marine-replic <86975170+marine-replic@users.noreply.github.com>
2022-04-08 16:22:50 -0400

    uibuild

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Person = require('./models/person')
 
 app.use(express.json())
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 // Morgan middleware for logging
 const morgan = require('morgan')
@@ -44,7 +23,9 @@ app.get('/', (request, response) => {
 
 // Get info for all entries
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 // Get info for one entry in the phonebook, error if entry not found
@@ -66,44 +47,40 @@ app.get('/api/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${total} people</p><p>${date}</p>`)
 })
 
-// Generate random id
-const generateId = () => {
-    id = Math.floor(Math.random() * 1000000000)
-    return id
-} 
-
 // Adding entry to phonebook
 app.post('/api/persons', (request, response) => {
-    const person = request.body
+    const body = request.body
 
     // Generate error if name or number is missing
-    if (!person.name) {
+    if (!body.name) {
         return response.status(400).json({
             error: 'name is missing'
         })
     } 
 
-    if (!person.number) {
+    if (!body.number) {
         return response.status(400).json({
             error: 'number is missing'
         })
     } 
 
-    // Verify if name already exists in the phonebook
-    const duplicateName = persons.map(p => p.name).includes(person.name)
-    if (duplicateName) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
+    // // Verify if name already exists in the phonebook
+    // const duplicateName = persons.map(p => p.name).includes(body.name)
+    // if (duplicateName) {
+    //     return response.status(400).json({
+    //         error: 'name must be unique'
+    //     })
+    // }
 
-    // Add new person to list of persons
-    person.id = generateId()
-    persons = persons.concat(person)
-    console.log(persons)
-
-    // Post new list
-    response.json(persons)
+    // Adding new person to MongoDB
+    const person = new Person({
+        name: body.name,
+        number: body.number,
+      })
+    
+      person.save().then(savedPerson => {
+        response.json(savedPerson)
+      })
 })
 
 // Deleting entry from phonebook
@@ -117,6 +94,6 @@ app.delete('/api/persons/:id', (request, response) => {
 
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
