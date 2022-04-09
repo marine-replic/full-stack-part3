3b71e52d9a00806fd47e63cc1ebc2c1a05d391c8
marine-replic <86975170+marine-replic@users.noreply.github.com>
2022-04-09 17:25:51 -0400

    uibuild

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*eslint linebreak-style: ["error", "windows"]*/
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -6,11 +7,11 @@ const Person = require('./models/person')
 
 
 const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
 }
 
 // json-parser middleware /!\ needs to be one of the first middleware logged
@@ -27,78 +28,78 @@ app.use(express.static('build'))
 
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+  response.send('<h1>Hello World!</h1>')
 })
 
 // Get info for all entries
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 // Get info for one entry in the phonebook, error if entry not found
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
-        .then(person => {
-            if (person) {
-                response.json(person)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 // Info page about phonebook
 app.get('/api/info', (request, response) => {
-    Person.find({}).count().then(total => {
-        const date = new Date()
-        response.send(`<p>Phonebook has info for ${total} people</p><p>${date}</p>`)
-    })
+  Person.find({}).count().then(total => {
+    const date = new Date()
+    response.send(`<p>Phonebook has info for ${total} people</p><p>${date}</p>`)
+  })
 })
 
 // Adding entry to phonebook
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    // Generate error if name or number is missing
-    if (!body.name) {
-        return response.status(400).json({
-            error: 'name is missing'
-        })
-    } 
+  // Generate error if name or number is missing
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name is missing'
+    })
+  }
 
-    if (!body.number) {
-        return response.status(400).json({
-            error: 'number is missing'
-        })
-    } 
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number is missing'
+    })
+  }
 
-    // // Verify if name already exists in the phonebook
-    // const duplicateName = persons.map(p => p.name).includes(body.name)
-    // if (duplicateName) {
-    //     return response.status(400).json({
-    //         error: 'name must be unique'
-    //     })
-    // }
+  // // Verify if name already exists in the phonebook
+  // const duplicateName = persons.map(p => p.name).includes(body.name)
+  // if (duplicateName) {
+  //     return response.status(400).json({
+  //         error: 'name must be unique'
+  //     })
+  // }
 
-    // Adding new person to MongoDB
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-      })
-    
-    person.save()
-        .then(savedPerson => {
-            response.json(savedPerson)
-        })
-        .catch(error => next(error))
+  // Adding new person to MongoDB
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 // Deleting entry from phonebook
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndRemove(request.params.id)
+  Person.findByIdAndRemove(request.params.id)
     .then(result => {
       response.status(204).end()
     })
@@ -108,23 +109,23 @@ app.delete('/api/persons/:id', (request, response, next) => {
 
 // Updating entry from phonebook
 app.put('/api/persons/:id', (request, response, next) => {
-    const { name, number } = request.body
-  
-    Person.findByIdAndUpdate(
-        request.params.id,
-        { name, number },
-        { new: true, runValidators: true, context: 'query' }
-    )
-        .then(updatedPerson => {
-            response.json(updatedPerson)
-        })
-        .catch(error => next(error))
+  const { name, number } = request.body
+
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 // Handler of requests with unknown endpoint
 // Must be placed 2nd to last or else every route will return 404
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
@@ -132,17 +133,17 @@ app.use(unknownEndpoint)
 // Handler of requests with result to errors
 // Must be placed last
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }  else if (error.code === 11000) {
-        return response.status(400).json({ error: 'duplicate name' })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }  else if (error.code === 11000) {
+    return response.status(400).json({ error: 'duplicate name' })
+  }
 
-    next(error)
+  next(error)
 }
 
 app.use(errorHandler)
