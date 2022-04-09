d6bc99376d5283776dc9168ba9e9aadab2299908
marine-replic <86975170+marine-replic@users.noreply.github.com>
2022-04-09 15:15:08 -0400

    uibuild

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
+const cors = require('cors')
+require('dotenv').config()
 const Person = require('./models/person')
 
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+// json-parser middleware /!\ needs to be one of the first middleware logged
 app.use(express.json())
 
-// Morgan middleware for logging
-const morgan = require('morgan')
-app.use(morgan('tiny'))
+// Error logging middleware
+app.use(requestLogger)
+
+// // Morgan middleware for logging
+// const morgan = require('morgan')
+// app.use(morgan('tiny'))
 
 // CORS middleware
-const cors = require('cors')
 app.use(cors())
 
 // Middleware to display static content from the build folder
@@ -29,22 +42,24 @@ app.get('/api/persons', (request, response) => {
 })
 
 // Get info for one entry in the phonebook, error if entry not found
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 // Info page about phonebook
 app.get('/api/info', (request, response) => {
-    const total = persons.length
-    const date = new Date()
-    response.send(`<p>Phonebook has info for ${total} people</p><p>${date}</p>`)
+    Person.find({}).count().then(total => {
+        const date = new Date()
+        response.send(`<p>Phonebook has info for ${total} people</p><p>${date}</p>`)
+    })
 })
 
 // Adding entry to phonebook
@@ -84,14 +99,52 @@ app.post('/api/persons', (request, response) => {
 })
 
 // Deleting entry from phonebook
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 
-    response.status(204).end()
 })
 
+// Updating entry from phonebook
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+  
+    const person = {
+      name: body.name,
+      number: body.number,
+    }
+  
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
+  })
 
+// Handler of requests with unknown endpoint
+// Must be placed 2nd to last or else every route will return 404
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+// Handler of requests with result to errors
+// Must be placed last
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT
